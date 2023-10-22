233c956bbf586a621efb526be45e1024cfd477fb
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-22 10:17:07 +0000

    Added search form

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,17 @@
 module SearchesHelper
+  def search_target
+    {
+      "User" => "user",
+      "Book" => "book"
+    }
+  end
+
+  def search_conditions
+    {
+      "完全一致" => "perfect",
+      "前方一致" => "forward",
+      "後方一致" => "backward",
+      "部分一致" => "partial"
+    }
+  end
 end

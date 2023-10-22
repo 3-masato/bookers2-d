c7e4697f8d7560377f3effc1ddc1308cb9981862
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-22 13:34:45 +0000

    Added search_for method by use the concerns

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,18 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+
   def search
+    @content = params[:content]
+    @target = params[:target]
+    @conditions = params[:conditions]
+
+    puts params
+
+    case @target
+    when "user"
+      @records = User.search_for(:name, @content, @conditions)
+    when "book"
+      @records = Book.search_for(:title, @content, @conditions)
+    end
   end
 end

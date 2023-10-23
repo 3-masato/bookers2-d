68630f501b3d02465f31123f8777c0a1a2047fc0
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-23 06:58:01 +0000

    Updated fav/comments features to async

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,16 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    fav = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    fav = current_user.favorites.new(book_id: @book.id)
     fav.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    fav = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    fav = current_user.favorites.find_by(book_id: @book.id)
     fav && fav.destroy # Countermeasures against unauthorized destroy actions
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 end
